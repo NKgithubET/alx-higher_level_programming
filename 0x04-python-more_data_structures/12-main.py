ff3ba0732db92f60be4420f679e5de940a129064
root <ets079711eeng@aastu.edu.et>
2024-01-11 04:54:30 -0800

    first commit

---
+++ 0x04-python-more_data_structures/12-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
